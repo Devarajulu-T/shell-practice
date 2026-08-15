a374393fc0526f7db312da8ba40e9453c21bb87b
Devaraj <devarajthota@gmail.com>
2026-08-15 10:05:25 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,5 +3,5 @@
 echo "Trump: Hey Musk, How are you?"
 echo "Musk: Hello Trump, I am fine. How are you doing"
 echo "Trump: I am fine too. What's up?"
-echo: "Musk: Nothing, just going to Mars now, will you come?"
-echo: "Turmp: Sorry, you carry on! I will come once you come back"
+echo "Musk: Nothing, just going to Mars now, will you come?"
+echo "Turmp: Sorry, you carry on! I will come once you come back"
