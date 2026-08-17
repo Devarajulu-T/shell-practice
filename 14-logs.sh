98a326e34a7370b20da6e8d3e3e3cf26e8abd514
Devaraj <devarajthota@gmail.com>
2026-08-17 05:33:04 +0530

    logs

--- 14-logs.sh
+++ 14-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/shellscript-logs"
-SCRIPTNAME=$(echo 14-logs.sh  | cut -d "." -f1)
+SCRIPTNAME=$(echo $? | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
